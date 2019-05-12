992841d6b1ee48ffa7747dff050033a5696e0216
ciechann <emgasuk@gmail.com>
2019-05-12 13:50:01 +0100

    min

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -2,12 +2,26 @@ package com.sda.algorytmy;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+
+
+        int[] a = {4, 6, 2, 1, 3};
+
+        System.out.println(min(a));
+    }
+
+    public static int min(int[] a) {
+        int wynik = a[1];
+
+        for (int i = 0; i <= a.length; i++) {
+            if (a[i] < wynik) {
+                wynik = a[i];
+            } else {
+                i++;
+            }
+        }
+        return wynik;
     }
 }
